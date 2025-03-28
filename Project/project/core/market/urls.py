cd49ecd8e99b0d173070e76e4690ed9b4d92d065
ErshkovDmitriy <ershkov.dmitrii@mail.ru>
2025-03-28 21:55:50 +0700

    доделал подвал, создал новые страницы для ссылок в навигации. перенес туда все ситили

--- Project/project/core/market/urls.py
+++ Project/project/core/market/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="base"),
+    path("about_project/", views.about_project, name="about_project"),
+    path("services/", views.services, name="services"),
+    path("partners/", views.partners, name="partners"),
+    path("contacts/", views.contacts, name="contacts"),
+
 ]
--- Project/project/core/market/views.py
+++ Project/project/core/market/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'base.html')
+
+def about_project(request):
+    return render(request, 'about_project.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def partners(request):
+    return render(request, 'partners.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
